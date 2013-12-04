2d348916ac44172560ec335b68403ba0cdeecc98
Tom <satom@debian>
2013-12-04 15:44:33 -0600

    Added documentation and license

--- lib/facter/rackconnect.rb
+++ lib/facter/rackconnect.rb
@@ -6,6 +6,22 @@
 # Caveats:
 #   Depends on Ruby json and rest-client modules
 #   Utilizes the is_rsc fact for confinement
+#
+# Copyright 2013 Rackspace
+# Initial revision Tom Noonan II
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
 
 if Facter.value('is_rsc') == "true"
   begin
@@ -24,6 +40,8 @@ if Facter.value('is_rsc') == "true"
     # If it throws an exception, then we're not.
     # Crude, but should be effective.
     begin
+      # http://www.rackspace.com/knowledge_center/article/the-rackconnect-api
+      # Authentication is host based; no passwords or tokens are needed.
       rcStatus = JSON.parse(RestClient.get("https://#{Facter.rsc_region}.api.rackconnect.rackspace.com/v1/automation_status?format=JSON", {:accept => :json}))
       is_rsc_rc = true
       Facter.add(:rsc_rc_status) do

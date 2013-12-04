3b1570c90de69767734d1c25e3486cc9fc59a86f
Tom <satom@debian>
2013-12-04 15:35:45 -0600

    Wrapped fact in a if is_rsc block to avoid failure messages on non-cloud servers

--- lib/facter/rackconnect.rb
+++ lib/facter/rackconnect.rb
@@ -7,36 +7,38 @@
 #   Depends on Ruby json and rest-client modules
 #   Utilizes the is_rsc fact for confinement
 
-begin
-  require 'rubygems'
-  require 'json'
-  require 'rest_client'
-  loadFailure = false
-rescue
-  loadFailure = true
-end
-
-# Avoiding nested exceptions
-if loadFailure == false and Facter.value('is_rsc') == "true"
-  # This is a somewhat janky way to populate the is_rsc_rc variable
-  # Basically, if the API knows us then we're RackConnected
-  # If it throws an exception, then we're not.
-  # Crude, but should be effective.
+if Facter.value('is_rsc') == "true"
   begin
-    rcStatus = JSON.parse(RestClient.get("https://#{Facter.rsc_region}.api.rackconnect.rackspace.com/v1/automation_status?format=JSON", {:accept => :json}))
-    is_rsc_rc = true
-    Facter.add(:rsc_rc_status) do
-      setcode do
-        rcStatus['automation_status']
-      end
-    end
+    require 'rubygems'
+    require 'json'
+    require 'rest_client'
+    loadFailure = false
   rescue
-    is_rsc_rc = false
+    loadFailure = true
   end
 
-  Facter.add(:is_rsc_rc) do
-    setcode do
-      is_rsc_rc
+  # Avoiding nested exceptions
+  if loadFailure == false
+    # This is a somewhat janky way to populate the is_rsc_rc variable
+    # Basically, if the API knows us then we're RackConnected
+    # If it throws an exception, then we're not.
+    # Crude, but should be effective.
+    begin
+      rcStatus = JSON.parse(RestClient.get("https://#{Facter.rsc_region}.api.rackconnect.rackspace.com/v1/automation_status?format=JSON", {:accept => :json}))
+      is_rsc_rc = true
+      Facter.add(:rsc_rc_status) do
+        setcode do
+          rcStatus['automation_status']
+        end
+      end
+    rescue
+      is_rsc_rc = false
+    end
+    
+    Facter.add(:is_rsc_rc) do
+      setcode do
+        is_rsc_rc
+      end
     end
   end
 end
